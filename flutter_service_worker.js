c175cf658d8dc2536fbdf4af9741098ebb0d1055
deep-diver <deep.diver.csp@gmail.com>
2021-06-23 12:51:47 +0000

    Deploying to gh-pages from @ deep-diver/image_search_with_natural_language@32485559ced11c3d227b52ddab7d4147c07c49eb 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,19 +3,19 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"version.json": "af738fa6743c73f4878b4bbfb2bd3e29",
+  "favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"main.dart.js": "fd553f1280e9cbe71b5ea45bd30dd3b4",
 "manifest.json": "dbb6c8a9f3e5640636734b3d6245cb8d",
+"index.html": "8300df466b4ccafd4e2bb1010d19c736",
+"/": "8300df466b4ccafd4e2bb1010d19c736",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "510e3f97c95a5254b18a053f46d8b582",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "fd553f1280e9cbe71b5ea45bd30dd3b4",
-"index.html": "7ec0509559099e5d6fc755a3d179d22f",
-"/": "7ec0509559099e5d6fc755a3d179d22f"
+"assets/NOTICES": "510e3f97c95a5254b18a053f46d8b582",
+"version.json": "af738fa6743c73f4878b4bbfb2bd3e29"
 };
 
 // The application shell files that are downloaded before a service worker can
