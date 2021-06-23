55b12ac70ddb34733cc416657329ad7a4144feb4
Chansung Park <deep.diver.csp@gmail.com>
2021-06-23 14:06:14 +0900

    Update api.py

--- server/api.py
+++ server/api.py
@@ -21,4 +21,4 @@ def get_images():
     return jsonify({"result": "good to go"})
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="127.0.0.1", port=8081, debug=True)
